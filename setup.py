2b7c43a4a0d31e8be8db9d7503d622db0cedc9c7
EdwardCoventry <eddcoventry@gmail.com>
2025-02-03 20:38:33 +0000

    trying to fix ignoring unknown option std:c++11

--- setup.py
+++ setup.py
@@ -5,27 +5,20 @@ import sys
 from setuptools import setup, Extension
 import platform
 
-# Read version from the source file (using neologdn.cpp as source)
+# Extract the version from neologdn.cpp
 with open('neologdn.cpp', 'r', encoding='utf8') as f:
     version = re.compile(r".*__version__ = '(.*?)'", re.S).match(f.read()).group(1)
 
-# Set the extra compile arguments based on the platform.
+# Set extra compile arguments based on the platform.
 if platform.system() == "Windows":
-    # Use MSVC flag for C++11 on Windows.
-    extra_compile_args = ["/std:c++11"]
+    # Use /std:c++14 instead of /std:c++11, as MSVC does not recognize /std:c++11.
+    extra_compile_args = ["/std:c++14"]
 else:
     extra_compile_args = ["-std=c++11"]
 
-# Add macOS-specific flags.
+# On macOS, add additional flags.
 if platform.system() == "Darwin":
     extra_compile_args.extend(["-mmacosx-version-min=10.7", "-stdlib=libc++"])
-
-# Define macros based on the Python version.
-# For Python 3.12 and 3.13, you can use the macro below inside your C++/Cython code
-# to select alternate implementations for removed or deprecated APIs.
-define_macros = []
-if sys.version_info >= (3, 12):
-    define_macros.append(('PYTHON3_12_OR_HIGHER', '1'))
 
 setup(
     name='neologdn',
@@ -38,8 +31,7 @@ setup(
             'neologdn',
             ['neologdn.cpp'],
             language='c++',
-            extra_compile_args=extra_compile_args,
-            define_macros=define_macros,
+            extra_compile_args=extra_compile_args
         )
     ],
     license='Apache Software License',
@@ -58,9 +50,6 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Topic :: Text Processing :: Linguistic'
     ],
     description='Japanese text normalizer for mecab-neologd',
     long_description='%s\n\n%s' % (
