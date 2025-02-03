0335b314524ff0ca520b74ae8d649d47b2705131
EdwardCoventry <eddcoventry@gmail.com>
2025-02-03 20:09:53 +0000

    updated for python 3.13

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
 # -*- coding: utf-8 -*-
 from codecs import open
 import re
+import sys
 from setuptools import setup, Extension
 import platform
 
+# Read version from the source file (using neologdn.cpp as source)
 with open('neologdn.cpp', 'r', encoding='utf8') as f:
-    version = re.compile(
-        r".*__version__ = '(.*?)'", re.S).match(f.read()).group(1)
+    version = re.compile(r".*__version__ = '(.*?)'", re.S).match(f.read()).group(1)
 
-extra_compile_args = ["-std=c++11"]
+# Set the extra compile arguments based on the platform.
+if platform.system() == "Windows":
+    # Use MSVC flag for C++11 on Windows.
+    extra_compile_args = ["/std:c++11"]
+else:
+    extra_compile_args = ["-std=c++11"]
+
+# Add macOS-specific flags.
 if platform.system() == "Darwin":
-    extra_compile_args.append("-mmacosx-version-min=10.7")
-    extra_compile_args.append("-stdlib=libc++")
+    extra_compile_args.extend(["-mmacosx-version-min=10.7", "-stdlib=libc++"])
+
+# Define macros based on the Python version.
+# For Python 3.12 and 3.13, you can use the macro below inside your C++/Cython code
+# to select alternate implementations for removed or deprecated APIs.
+define_macros = []
+if sys.version_info >= (3, 12):
+    define_macros.append(('PYTHON3_12_OR_HIGHER', '1'))
 
 setup(
     name='neologdn',
@@ -19,8 +33,15 @@ setup(
     author='Yukino Ikegami',
     author_email='yknikgm@gmail.com',
     url='http://github.com/ikegami-yukino/neologdn',
-    ext_modules=[Extension('neologdn', ['neologdn.cpp'],
-                           language='c++', extra_compile_args=extra_compile_args)],
+    ext_modules=[
+        Extension(
+            'neologdn',
+            ['neologdn.cpp'],
+            language='c++',
+            extra_compile_args=extra_compile_args,
+            define_macros=define_macros,
+        )
+    ],
     license='Apache Software License',
     keywords=['japanese', 'MeCab'],
     classifiers=[
@@ -37,10 +58,13 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Text Processing :: Linguistic'
     ],
     description='Japanese text normalizer for mecab-neologd',
-    long_description='%s\n\n%s' % (open('README.rst', 'r', encoding='utf8').read(),
-                                   open('CHANGES.rst', 'r', encoding='utf8').read()
-                                   ),
+    long_description='%s\n\n%s' % (
+        open('README.rst', 'r', encoding='utf8').read(),
+        open('CHANGES.rst', 'r', encoding='utf8').read()
+    ),
 )
