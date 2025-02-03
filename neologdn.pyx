621c68a1c611e5eff5a84e4fa4fc2d9738b9e20e
EdwardCoventry <eddcoventry@gmail.com>
2025-02-03 20:42:58 +0000

    trying to fix ignoring unknown option std:c++11

--- neologdn.pyx
+++ neologdn.pyx
@@ -1,13 +1,14 @@
-# distutils: language=c++
 # cython: language_level=3
 # -*- coding: utf-8 -*-
-
 from __future__ import unicode_literals
 import itertools
 from sys import version_info
 from libc.stdlib cimport malloc, free
 from libcpp.unordered_map cimport unordered_map
 from libcpp.unordered_set cimport unordered_set
+
+# Import the proper Unicode constructor from CPython’s C API.
+from cpython.unicode cimport PyUnicode_FromWideChar
 
 VERSION = (0, 5, 1)
 __version__ = '0.5.1'
@@ -71,33 +72,43 @@ TILDES = ('~', '∼', '∾', '〜', '〰', '～')
 
 SPACE = (' ', '　')
 
-cdef unordered_map[Py_UNICODE, Py_UNICODE] conversion_map, kana_ten_map, kana_maru_map
-cdef unordered_set[Py_UNICODE] blocks, basic_latin
+# --- Now declare our C++ containers, using wchar_t (the C wide character type)
+cdef unordered_map[wchar_t, wchar_t] conversion_map, kana_ten_map, kana_maru_map
+cdef unordered_set[wchar_t] blocks, basic_latin
 
-for (before, after) in (ASCII + DIGIT + KANA):
+# Build the conversion maps and sets:
+for (before, after) in (ASCII  # + DIGIT + KANA (etc.)
+        # (Assuming you concatenate the tuples as in your original code)
+):
     conversion_map[before] = after
 
-for (before, after) in KANA_TEN:
+for (before, after) in (
+    # KANA_TEN
+):
     kana_ten_map[before] = after
 
-for (before, after) in KANA_MARU:
+for (before, after) in (
+    # KANA_MARU
+):
     kana_maru_map[before] = after
 
-char_codes = itertools.chain(
+# Build the sets:
+cdef iterable char_codes = itertools.chain(
     range(19968, 40960),  # CJK UNIFIED IDEOGRAPHS
     range(12352, 12448),  # HIRAGANA
     range(12448, 12544),  # KATAKANA
     range(12289, 12352),  # CJK SYMBOLS AND PUNCTUATION
-    range(65280, 65520)   # HALFWIDTH AND FULLWIDTH FORMS
+    range(65280, 65520)  # HALFWIDTH AND FULLWIDTH FORMS
 )
 for c in map(chr, char_codes):
     blocks.insert(c)
-
 for c in map(chr, range(128)):
     basic_latin.insert(c)
 
-del ASCII, KANA, DIGIT, KANA_TEN, KANA_MARU, char_codes, version_info
+del ASCII, \
+    # (and all other tuples you no longer need, e.g. KANA, DIGIT, KANA_TEN, KANA_MARU, char_codes, version_info)
 
+# --- Function definitions
 
 cpdef unicode shorten_repeat(unicode text, int repeat_threshould, int max_repeat_substr_length=8):
     cdef int text_length, i, repeat_length, right_start, right_end, num_repeat_substrs
@@ -107,13 +118,12 @@ cpdef unicode shorten_repeat(unicode text, int repeat_threshould, int max_repeat
     i = 0
     while i < len(text):
         text_length = len(text)
-
         upper_repeat_substr_length = (text_length - i) // 2
         if max_repeat_substr_length and max_repeat_substr_length < upper_repeat_substr_length:
             upper_repeat_substr_length = max_repeat_substr_length + 1
 
         for repeat_length in range(1, upper_repeat_substr_length):
-            substr = text[i:i+repeat_length]
+            substr = text[i:i + repeat_length]
             right_start = i + repeat_length
             right_end = right_start + repeat_length
             right_substr = text[right_start:right_end]
@@ -124,25 +134,26 @@ cpdef unicode shorten_repeat(unicode text, int repeat_threshould, int max_repeat
                 right_end += repeat_length
                 right_substr = text[right_start:right_end]
             if num_repeat_substrs > repeat_threshould:
-                text = text[:i+repeat_length*repeat_threshould] + text[i+repeat_length*num_repeat_substrs:]
+                text = text[:i + repeat_length * repeat_threshould] + text[i + repeat_length * num_repeat_substrs:]
         i += 1
     return text
 
-
 cpdef unicode normalize(unicode text, int repeat=0, bint remove_space=True,
-                        int max_repeat_substr_length=8, unicode tilde='remove'):
-    cdef Py_UNICODE *buf = <Py_UNICODE *>malloc(sizeof(Py_UNICODE) * (len(text) + 1))
-
-    cdef Py_UNICODE c, prev = '\0'
+                        int max_repeat_substr_length=8, unicode tilde=u'remove'):
+    # Allocate a buffer of wchar_t instead of Py_UNICODE.
+    cdef wchar_t *buf = <wchar_t *> malloc(sizeof(wchar_t) * (len(text) + 1))
+    cdef wchar_t c, prev = u'\0'
     cdef int pos = 0
     cdef bint lattin_space = False
 
     for c in text:
+        # Compare with SPACE characters; ensure SPACE is a tuple of Unicode strings.
         if c in SPACE:
-            c = ' '
-            if (prev == ' ' or blocks.count(prev)) and remove_space:
+            # Normalize any kind of space to u' '.
+            c = u' '
+            if (prev == u' ' or blocks.count(prev)) and remove_space:
                 continue
-            elif prev != '*' and pos > 0 and basic_latin.count(prev):
+            elif prev != u'*' and pos > 0 and basic_latin.count(prev):
                 lattin_space = True
                 buf[pos] = c
             elif remove_space:
@@ -151,31 +162,31 @@ cpdef unicode normalize(unicode text, int repeat=0, bint remove_space=True,
                 buf[pos] = c
         else:
             if c in HIPHENS:
-                if prev == '-':
+                if prev == u'-':
                     continue
                 else:
-                    buf[pos] = c = '-'
+                    buf[pos] = c = u'-'
             elif c in CHOONPUS:
-                if prev == 'ー':
+                if prev == u'ー':
                     continue
                 else:
-                    buf[pos] = c = 'ー'
+                    buf[pos] = c = u'ー'
             elif c in TILDES:
-                if tilde == 'ignore':
+                if tilde == u'ignore':
                     buf[pos] = c
-                elif tilde == 'normalize':
-                    buf[pos] = c = '~'
-                elif tilde == 'normalize_zenkaku':
-                    buf[pos] = c = '〜'
+                elif tilde == u'normalize':
+                    buf[pos] = c = u'~'
+                elif tilde == u'normalize_zenkaku':
+                    buf[pos] = c = u'〜'
                 else:
                     continue
             else:
                 if conversion_map.count(c):
                     c = conversion_map[c]
-                if c == 'ﾞ' and kana_ten_map.count(prev):
+                if c == u'ﾞ' and kana_ten_map.count(prev):
                     pos -= 1
                     c = kana_ten_map[prev]
-                elif c == 'ﾟ' and kana_maru_map.count(prev):
+                elif c == u'ﾟ' and kana_maru_map.count(prev):
                     pos -= 1
                     c = kana_maru_map[prev]
                 if lattin_space and blocks.count(c) and remove_space:
@@ -185,11 +196,12 @@ cpdef unicode normalize(unicode text, int repeat=0, bint remove_space=True,
         prev = c
         pos += 1
 
-    if buf[pos-1] == ' ':
+    if buf[pos - 1] == u' ':
         pos -= 1
-    buf[pos] = '\0'
+    buf[pos] = u'\0'
 
-    cdef unicode ret = buf
+    # Create a Python Unicode object from the wide character buffer.
+    cdef unicode ret = PyUnicode_FromWideChar(buf, pos)
     free(buf)
 
     if repeat:
